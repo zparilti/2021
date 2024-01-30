c282513cd633aba923b513e1c58aa441ecd2dcfe
zparilti <67382808+zparilti@users.noreply.github.com>
2024-01-30 13:06:50 -0500

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -6,3 +6,6 @@ class BankAccount:
         self.balance = initialAmount
         self.name = acctName
         print(f"\nAccount '{self.name}' created.\nBalance = ${self.balance}")
+
+Dave = BankAccount(1000, "Dave")
+Sara = BankAccount(2000, "Sara")
