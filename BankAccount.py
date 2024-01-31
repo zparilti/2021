8573e9a019526d2834ab2afc9e1a8d15a4521a98
zparilti <67382808+zparilti@users.noreply.github.com>
2024-01-31 14:02:17 -0500

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,13 @@
 # Python Program explaining OOP 
 
+# initialize BankAccount object
 class BankAccount:
     def __init__(self, initialAmount, acctName):
         self.balance = initialAmount
         self.name = acctName
         print(f"\nCustomer accounts created.\nAccount Name: {self.name}\nBalance = ${self.balance:.2f}")
-      
+
+# initialize instances of BankAccount object      
 customer_BankAccount1 = BankAccount(1000, "David")
 customer_BankAccount1 = BankAccount(2000, "Shari")
+
